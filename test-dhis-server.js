d6aebfcf1e2154d1362e3977a54020003492f323
Ryan Crichton <rg.crichton@gmail.com>
2016-02-10 17:15:00 +0200

    Enable this mediator to do verification only
    Via a mediator config option the user may specify whether the mediator
    will transform requests by replacing IDs or if it will only verify IDs
    used in the message without doing any transformation.

--- test-dhis-server.js
+++ test-dhis-server.js
@@ -5,6 +5,7 @@ const http = require('http');
 
 const sucessfulResponse = 'CORRECT CODES USED';
 const failedResponse = 'INCORRECT CODES USED';
+const failedResponse2 = 'ORIGINAL CODES USED';
 
 const server = http.createServer(function (req, res) {
   let body = '';
@@ -17,6 +18,9 @@ const server = http.createServer(function (req, res) {
     if (body.indexOf('orgUnit="123"') > 0 && body.indexOf('orgUnit="456"') > 0) {
       res.writeHead(200);
       res.end(sucessfulResponse);
+    } else if (body.indexOf('orgUnit="p.ao.pepfar.3"') > 0 && body.indexOf('orgUnit="p.ao.pepfar.44"') > 0) {
+      res.writeHead(200);
+      res.end(failedResponse2);
     } else {
       res.writeHead(400);
       res.end(failedResponse);
